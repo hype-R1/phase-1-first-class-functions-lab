188eb9aef9837ea653f152142bdcb0e9e2d10b1b
Michael <mjpj@live.com>
2021-11-03 20:22:25 -0400

    completed

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-const returnFirstTwoDrivers = function (drivers) {
-    return drivers.slice(0, 2);
-  };
-  
-  const returnLastTwoDrivers = function (drivers) {
-    return drivers.slice(-2);
-  };
-  
-  const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers ];
-  
-  const createFareMultiplier = function (multiplyValue) {
-    return function (value) {
-      return multiplyValue * value;
-    };
-  };
-  
-  const fareDoubler = createFareMultiplier(2);
-  
-  const fareTripler = createFareMultiplier(3);
-  
-  const selectDifferentDrivers = function (drivers, driversToReturn) {
-    return driversToReturn(drivers);
-  };
+const returnFirstTwoDrivers = (drivers) => {return drivers.slice(0, 2)}
+const returnLastTwoDrivers = (drivers) => {return drivers.slice(-2)}
+const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers ];
+const createFareMultiplier = (multiplyValue) => function (x) {
+  return multiplyValue * x}
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+const selectDifferentDrivers = (drivers, driversToReturn) => driversToReturn(drivers);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
